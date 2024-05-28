d2693a09e08d6ff0943fcfed2e32ef340fcfc046
Andrea Jao <ndrjao@gmail.com>
2024-05-28 11:45:48 -0700

    added app and eight ball styling

--- react-modules/src/App.jsx
+++ react-modules/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-import logo from './logo.svg'
+import EightBall from "./EightBall"
 import './App.css';
-
 
 /** Component for entire page.
  *
@@ -11,27 +9,11 @@ import './App.css';
 */
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function incrCount() {
-    setCount(count => count + 1);
-  }
 
   return (
     <div className="App">
       <main>
-        <img src={logo} className="App-logo" alt="Rithm" />
-        <h1>Rithm React Starter</h1>
-        <p>
-          <button
-              className="btn btn-primary"
-              onClick={incrCount}>
-            Clicked: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>src/App.jsx</code>
-        </p>
+        <EightBall />
       </main>
     </div>
   );
--- react-modules/src/EightBall.jsx
+++ react-modules/src/EightBall.jsx
@@ -1,4 +1,5 @@
 import {shuffle} from "lodash"
+import './EightBall.css';
 /**
  * Displays a colored circle with an answer
  *
@@ -10,7 +11,30 @@ import {shuffle} from "lodash"
  * App -> EightBall
  */
 
-function EightBall({answers}){
+const defaultAnswers = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+]
+
+function EightBall({answers=defaultAnswers}){
 
     const [answer, setAnswer] = useState({
          msg: "Think of a Question.", color: "black" })
@@ -29,3 +53,5 @@ function EightBall({answers}){
         </div>
     )
 }
+
+export default EightBall
